f1cdfa124f0f149ce7cf10b859ba01eaac88cb0c
Harry Xue <wxue@college.harvard.edu>
2015-07-08 15:59:28 -0400

    Pushing so we both can work on this

--- hx_annotations_lti/urls.py
+++ hx_annotations_lti/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import django_app_lti.urls
+
 admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
@@ -10,7 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^lti_init/', include('hx_lti_initializer.urls', namespace="hx_lti_intializer")),
     url(r'^lti_init/launch_lti/annotation/', include('hx_lti_todapi.urls', namespace="hx_lti_todapi")),
-    url(r'^accounts/profile/', TemplateView.as_view(template_name='index.html')) 
+    url(r'^accounts/profile/', TemplateView.as_view(template_name='index.html')),
+    url(r'^lti/', include(django_app_lti.urls, namespace="lti")) 
 )
 
 urlpatterns += staticfiles_urlpatterns()
