e56f90d278e920e52d44bbc53d82aab0331dfb77
Christian Ondaatje <christian.ondaatje@gmail.com>
2015-07-13 16:59:31 -0400

    synch with harry

--- hx_annotations_lti/urls.py
+++ hx_annotations_lti/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'hx_annotations_lti.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^lti_init/', include('hx_lti_initializer.urls', namespace="hx_lti_intializer")),
+    url(r'^lti_init/', include('hx_lti_initializer.urls', namespace="hx_lti_initializer")),
     url(r'^lti_init/launch_lti/annotation/', include('hx_lti_todapi.urls', namespace="hx_lti_todapi")),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='index.html')),
     url(r'^lti/', include(django_app_lti.urls, namespace="lti"))
