d6aeb99ff793e770df5a174b017b83069a9482a5
Arthur Barrett <abarrett@fas.harvard.edu>
2015-08-13 10:19:46 -0400

    Fixed BASE_DIR

--- annotationsx/settings/aws.py
+++ annotationsx/settings/aws.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 from django.contrib import messages
 from secure import SECURE_SETTINGS
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
