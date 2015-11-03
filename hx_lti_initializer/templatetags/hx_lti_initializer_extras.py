ff1d5039f1e5d9f64de7b23e422266d6ac9ea51e
lduarte1991 <lduarte1991@gmail.com>
2015-11-03 16:11:22 +0000

    Fixed instructor dashboard to include image media types

--- hx_lti_initializer/templatetags/hx_lti_initializer_extras.py
+++ hx_lti_initializer/templatetags/hx_lti_initializer_extras.py
@@ -8,8 +8,10 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from re import sub
 
 from hx_lti_assignment.models import Assignment
+from hx_lti_initializer.utils import debug_printer
 from abstract_base_classes.target_object_database_api import TOD_Implementation
 from target_object_database.models import TargetObject
+import re
 
 def convert_tz(datetimeobj):
 	'''
@@ -71,19 +73,51 @@ def get_target_object_name(object_id):
 		return unicode(targ_obj)
 	# TODO: Currently fails silently. Eventually we will want to improve the model design so this doesn't happen.
 	except:
-		return ''
+		try:
+			# if it is an image, the object_id will not match as it follows mirador conventions
+			trimmed_object_id = re.sub(r'/canvas/.*', '', object_id)
+			target_obj = TargetObject.objects.filter(target_content__icontains=trimmed_object_id)[0]
+			return unicode(target_obj)
+		except:
+			debug_printer("Failed:%s" % object_id)
+			return ''
+
+@register.simple_tag
+def get_target_id(object_id):
+	try:
+		targ_obj = TargetObject.objects.get(pk=object_id)
+		return targ_obj.id
+	# TODO: Currently fails silently. Eventually we will want to improve the model design so this doesn't happen.
+	except:
+		try:
+			# if it is an image, the object_id will not match as it follows mirador conventions
+			trimmed_object_id = re.sub(r'/canvas/.*', '', object_id)
+			target_obj = TargetObject.objects.filter(target_content__icontains=trimmed_object_id)[0]
+			return target_obj.id
+		except:
+			debug_printer("Failed:%s" % object_id)
+			return ''
 
 @register.assignment_tag
 def assignment_object_exists(object_id, collection_id):
 	'''
 		Checks to make sure that both the assignment and the target object exist
 	'''
+	
 	try:
 		targ_obj = TargetObject.objects.get(pk=object_id)
-		assignment = Assignment.objects.get(assignment_id=collection_id);
+		assignment = Assignment.objects.get(assignment_id=collection_id)
 		return True
 	except:
-		return False
+		try:
+			# if it is an image, the object_id will not match as it follows mirador conventions
+			trimmed_object_id = re.sub(r'/canvas/.*', '', object_id)
+			target_obj = TargetObject.objects.filter(target_content__icontains=trimmed_object_id)[0]
+			assignment = Assignment.objects.get(assignment_id=collection_id)
+			return True
+		except:
+			debug_printer("Failed:%s" % object_id)
+			return False
 
 @register.simple_tag
 def get_annotation_by_id(annotation_id, annotations):
