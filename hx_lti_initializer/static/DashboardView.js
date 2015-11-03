7a3f5eaa536c382544ff0ca72e351feebdcaf31c
Arthur Barrett <abarrett@fas.harvard.edu>
2015-11-03 15:28:17 -0500

    Fixed whitespace.

--- hx_lti_initializer/static/DashboardView.js
+++ hx_lti_initializer/static/DashboardView.js
@@ -155,7 +155,7 @@
         if (item.media === "image" && item.thumb) {
             item.thumbnail = item.thumb;
         }
-		item.tags = item.tags || [];
+        item.tags = item.tags || [];
         return item;
     };
 
