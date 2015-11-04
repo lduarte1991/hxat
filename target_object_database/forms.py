8ac8b8a6d092c8b42c8fe957d33b54b721600d6f
lduarte1991 <lduarte1991@gmail.com>
2015-11-04 21:21:48 +0000

    Limiting Manifests URL to 1 per Target Object + enforce 1-to-1 relationship between the two.

--- target_object_database/forms.py
+++ target_object_database/forms.py
@@ -1,8 +1,23 @@
 from django import forms
 from target_object_database.models import TargetObject
+from hx_lti_initializer.utils import debug_printer
 
 
 class SourceForm(forms.ModelForm):
+
+    def clean(self):
+        super(SourceForm, self).clean()
+        data = self.cleaned_data.get('target_content')
+        targ_type = self.cleaned_data.get('target_type')
+
+        possible_to = None
+        try:
+            possible_to = TargetObject.objects.filter(target_content__icontains=data)[0]
+        except:
+            pass
+        if possible_to is not None:
+            msg = "This image manifest already exists and is named %s" % possible_to
+            self.add_error('target_content', msg)
 
     class Meta:
         model = TargetObject
