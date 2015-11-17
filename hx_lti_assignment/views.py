5c413b113ef01c7641fed3a0cfbf3d2041509cfe
Arthur Barrett <abarrett@fas.harvard.edu>
2015-11-17 16:00:00 -0500

    Allow the default pagination limit to be configured in the settings, so different environments can easily set different defaults.

--- hx_lti_assignment/views.py
+++ hx_lti_assignment/views.py
@@ -85,8 +85,7 @@ def create_new_assignment(request):
                 'annotation_database_url': getattr(settings, 'ANNOTATION_DB_URL', ""),
 	            'annotation_database_apikey': getattr(settings, 'ANNOTATION_DB_API_KEY', ""),
 	            'annotation_database_secret_token': getattr(settings, 'ANNOTATION_DB_SECRET_TOKEN', ""),
-	            # TODO: Figure out a sensible default
-	            'pagination_limit': 20,
+	            'pagination_limit': getattr(settings, 'ANNOTATION_PAGINATION_LIMIT_DEFAULT', 20),
             })
         targets_form = AssignmentTargetsFormSet()
         target_num = 0
