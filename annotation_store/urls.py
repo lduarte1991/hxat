d593e899de1dbdf79158d2b60e2346e9f90eadde
lduarte1991 <lduarte1991@gmail.com>
2019-04-16 20:32:32 +0000

    changing issues in urls.py files

--- annotation_store/urls.py
+++ annotation_store/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-import views
+from . import views
 
 urlpatterns = [
     url( r'^api$', views.api_root, name="api_root"),
--- annotationsx/urls.py
+++ annotationsx/urls.py
@@ -7,14 +7,14 @@ import django_app_lti.urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^lti_init/', include('hx_lti_initializer.urls', namespace="hx_lti_initializer")),
-    url(r'^annotation_store/', include('annotation_store.urls', namespace="annotation_store")),
-    url(r'^lti_init/launch_lti/assignment/', include('hx_lti_assignment.urls', namespace="hx_lti_assignment")),
-    url(r'^lti_init/launch_lti/', include('target_object_database.urls', namespace="target_object_database")),
+    url(r'^lti_init/', include(('hx_lti_initializer.urls', 'hx_lti_initializer.urls'), namespace="hx_lti_initializer")),
+    url(r'^annotation_store/', include(('annotation_store.urls', 'annotation_store.urls'), namespace="annotation_store")),
+    url(r'^lti_init/launch_lti/assignment/', include(('hx_lti_assignment.urls', 'hx_lti_assignment.urls'), namespace="hx_lti_assignment")),
+    url(r'^lti_init/launch_lti/', include(('target_object_database.urls', 'target_object_database.urls'), namespace="target_object_database")),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='index.html')),
     url(r'^500/', TemplateView.as_view(template_name="main/500.html")),
     url(r'^troubleshooting/', TemplateView.as_view(template_name="main/troubleshooting.html")),
     # TODO: Check to see if this works without enabling django_app_lti
     # Include the lti app's urls
-    url(r'^lti/', include(django_app_lti.urls, namespace="lti")),
+    url(r'^lti/', include((django_app_lti.urls, 'lti'), namespace="lti")),
 ]
