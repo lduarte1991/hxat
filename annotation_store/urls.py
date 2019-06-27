20f48ba17704011106b3616049955b0648757b9d
lduarte1991 <lduarte1991@gmail.com>
2019-06-27 17:21:08 +0000

    Connected Hxighlighter to CATCHPy successfully

--- annotation_store/urls.py
+++ annotation_store/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url( r'^api$', views.api_root, name="api_root"),
+    url( r'^api/(?P<annotation_id>[A-Za-z0-9-]+|)?$', views.api_root, name="api_root"),
     url( r'^api/search$',views.search,name="api_search"),
     url( r'^api/create$', views.create, name="api_create"),
     url( r'^api/delete/(?P<annotation_id>[0-9]+|)$', views.delete, name="api_delete"),
