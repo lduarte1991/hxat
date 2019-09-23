a4455e94a35cc0f4b576e9b08282ada92bad9b5a
lduarte1991 <lduarte1991@gmail.com>
2019-09-23 11:04:00 -0400

    2019 09 23 grading hotfix 2 (#110)
    * adding a utf-8 decoder

--- annotation_store/store.py
+++ annotation_store/store.py
@@ -376,7 +376,7 @@ class CatchStoreBackend(StoreBackend):
 
     def after_search(self, response):
         retrieved_self = self.request.LTI['launch_params'].get('user_id', '*') == self.request.GET.get('user_id', '')
-        return retrieved_self and int(json.loads(str(response.content))['total'] > 0)
+        return retrieved_self and int(json.loads(str(response.content).decode('utf-8'))['total'] > 0)
 
     def create(self, annotation_id):
         body = self._get_request_body()
@@ -647,7 +647,7 @@ class WebAnnotationStoreBackend(StoreBackend):
 
     def after_search(self, response):
         retrieved_self = self.request.LTI['launch_params'].get('user_id', '*') in self.request.GET.getlist('userid[]', [])
-        return retrieved_self and int(json.loads(str(response.content))['total'] > 0)
+        return retrieved_self and int(json.loads(str(response.content.decode('utf-8')))['total'] > 0)
 
     def create(self, annotation_id):
         body = self._get_request_body()
