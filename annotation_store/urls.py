93b7762529e4ca380f6d06aaf9faa4342a6eaaa8
lduarte1991 <lduarte1991@gmail.com>
2019-11-14 16:52:35 -0500

    2019 11 13 grading bug fix (#113)
    * created "grade me" api url that can be hooked into via a button in the UI to passback grade via lti
    * fixed bug where old UI versions were not sending grades at all
    * changed urrlib call for python 3
    * changing params to match catchpy new api
    * Adding tooltip to confirm grade
    * added check to only add grade me if assignment is being graded

--- annotation_store/urls.py
+++ annotation_store/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url( r'^api/destroy/(?P<annotation_id>[0-9]+|)$', views.delete, name="api_delete"),
     url( r'^api/update/(?P<annotation_id>[0-9]+)$', views.update, name="api_update"),
     url( r'^api/transfer_annotations/(?P<instructor_only>[0-1])?$', views.transfer, name="api_transfer_annotations"),
+    url( r'^api/grade/me', views.grade_me, name="api_grade_me"),
 ]
