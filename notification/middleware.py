d41bfb1d7ae7963c14b01e11a22e0cf8a3e1ffdc
nmaekawa <nmaekawa@g.harvard.edu>
2020-02-12 17:39:07 -0500

    wstoken: wip many debug messages for session

--- notification/middleware.py
+++ notification/middleware.py
@@ -5,6 +5,7 @@ from importlib import import_module
 from urllib.parse import parse_qs
 
 from django.conf import settings
+from django.contrib.sessions.models import Session
 from django.db import close_old_connections
 
 
@@ -44,13 +45,46 @@ class SessionAuthMiddleware(object):
             resource_link_id))
 
         if session_id is not None and resource_link_id is not None:
+            self.log.debug('******************** sid and rid NOT NONE')
             session = SessionStore(session_id)
+            # -------------------------------------------------
+            '''
+            try:
+                session = Session.objects.get(pk=session_id)
+                self.log.debug('******************** session[{}] = ({})'.format(type(session), session))
+                for k,v in vars(session).items():
+                    self.log.debug('******************** session[{}] = ({})'.format(k,v))
+
+            except DoesNotExist as e:
+                self.log.debug('******************** unknown session-id({})'.format(session_id))
+                scope['hxat_auth'] = 'forbidden'
+
+
+            s = session.get_decoded()
+            self.log.debug('******************** SESSION({}) len({})'.format(s, len(s)))
+            for key, value in s.items():
+                self.log.debug('******************** SESSION[{}] = {}'.format(key, value))
+
+
+            '''
+            # -------------------------------------------------
             if session.exists(session_id):
-                lti_obj = session.get(resource_link_id, None)
+                for attr, value in vars(session).items():
+                    self.log.debug('******************** SESSION[{}] = {}'.format(attr, value))
+
+                lti_launch = session.get('LTI_LAUNCH', {})
+                for attr, value in lti_launch.items():
+                    self.log.debug('******************** LTI_LAUTCH[{}] = {}'.format(attr, value))
+
+                lti_obj = lti_launch.get(resource_link_id, None)
                 if lti_obj is not None:
                     for key in lti_obj.keys():
                         self.log.debug('******************** lti-session[{}] = {}'.format(key, lti_obj[key]))
                     scope['hxat_auth'] = 'authenticated'
+                else:
+                    self.log.debug(
+                            '******************** session({}) lti_obj({})'.format(session, lti_obj))
+
             else:
                 # forbidden: unknown session id
                 self.log.debug('******************** unknown session-id({})'.format(session_id))
