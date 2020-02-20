a1f1d83cc06dc628e98ce5569c89fee226fd25f3
nmaekawa <nmaekawa@g.harvard.edu>
2020-02-20 12:32:47 -0500

    wstoken: ws unit tests, adds group after ok ws conn
    - adds group to channel after accepting ws conn
    - better error msg in scope for notification consumer
    - notification unit tests depend on lti-launch

--- notification/middleware.py
+++ notification/middleware.py
@@ -17,7 +17,6 @@ class SessionAuthMiddleware(object):
     """auth via session_id in query string."""
 
 
-
     def __init__(self, inner):
         self.inner = inner
         self.log = logging.getLogger(__name__)
@@ -29,7 +28,7 @@ class SessionAuthMiddleware(object):
                 scope.get(key)))
 
         # not authorized yet
-        scope['hxat_auth'] = 'forbidden'
+        scope['hxat_auth'] = '403'
 
         # parse path to get context_id, collection_id, target_source_id
         path = scope.get('path')
@@ -37,29 +36,34 @@ class SessionAuthMiddleware(object):
         (context, collection, target) = room_name.split('--')
         self.log.debug('******************** context({}) collection({}) target({})'.format(context, collection, target))
 
+        # parse query string for session-id and resource-link-id
         query_string = scope.get('query_string', '')
         parsed_query = parse_qs(query_string)
         self.log.debug('******************** parsed({})'.format(parsed_query)),
 
         if not parsed_query:
             self.log.debug('******************** finished with middleware EARLY: no query string')
+            scope['hxat_auth'] = '403: missing querystring'
             return self.inner(scope)
 
-        session_id = parsed_query.get(b'utm_source', None)[0].decode()
-        resource_link_id = parsed_query.get(b'resource_link_id', None)[0].decode()
+        session_id = parsed_query.get(b'utm_source', [b''])[0].decode()
+        resource_link_id = parsed_query.get(b'resource_link_id', [b''])[0].decode()
 
         self.log.debug('******************** sid({}) rid({})'.format(session_id,
             resource_link_id))
 
-        if session_id is None or resource_link_id is None:
-            # forbidden: missing session id in query string
-            self.log.debug('******************** missing session-id or resource-link-id')
+        if not session_id or not resource_link_id:
+            scope['hxat_auth'] = '403: missing session-id or resource-link-id'
+            self.log.debug('******************** {}'.format(scope['hxat_auth']))
             return self.inner(scope)
 
+        # close old db conn to prevent usage of timed out conn
+        # see https://channels.readthedocs.io/en/latest/topics/authentication.html#custom-authentication
+        close_old_connections()
         session = SessionStore(session_id)
         if not session.exists(session_id):
-            # forbidden: unknown session id
-            self.log.debug('******************** unknown session-id({})'.format(session_id))
+            scope['hxat_auth'] = '403: unknown session-id({})'.format(session_id)
+            self.log.debug('******************** {}'.format(scope['hxat_auth']))
         else:
             multi_launch = session.get('LTI_LAUNCH', {})
             lti_launch = multi_launch.get(resource_link_id, {})
@@ -67,9 +71,11 @@ class SessionAuthMiddleware(object):
 
             # check the context-id matches the channel being connected
             clean_context_id = re.sub('[\W_]', '-', lti_params.get('context_id', ''))
-            self.log.debug('******************** context_id[{}] = {}'.format(clean_context_id, context))
             if clean_context_id == context:
                 scope['hxat_auth'] = 'authenticated'
+            else:
+                scope['hxat_auth'] = '403: unknown context-id({})'.format(context)
+                self.log.debug('******************** {}'.format(scope['hxat_auth']))
 
         self.log.debug('******************** finished with middleware')
         return self.inner(scope)
