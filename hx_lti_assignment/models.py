64979403ae16fce3f59d187868d2c7cae5df0bd0
lduarte1991 <lduarte1991@gmail.com>
2020-03-16 10:04:27 -0400

    logging order

--- hx_lti_assignment/models.py
+++ hx_lti_assignment/models.py
@@ -242,6 +242,7 @@ class Assignment(models.Model):
                     assignment=self,
                     target_object=obj
                 )
+                logger.debug("CURRENT_ORDER: %d - Total: %d" % (assignment_target.order, len(self.assignment_objects.all())))
                 if assignment_target.order == len(self.assignment_objects.all()):  # noqa
                     return None
                 else:
