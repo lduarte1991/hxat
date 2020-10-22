1cc12773ccdc60627502da2b9812265af47632b7
nmaekawa <nmaekawa@g.harvard.edu>
2020-10-22 17:37:28 -0400

    flake8: abstract_base_classes after tweaks

--- abstract_base_classes/target_object_database_api.py
+++ abstract_base_classes/target_object_database_api.py
@@ -1,13 +1,13 @@
 """
-    This is the file that should change when talking about changing 
+    This is the file that should change when talking about changing
     Target Object Databases! Change the Current_Implementation functions to call
-    whatever functions you are currently using. This will allow you to keep the 
-    API intact as well as automatically run tests. 
+    whatever functions you are currently using. This will allow you to keep the
+    API intact as well as automatically run tests.
 
 """
-from abc import *
+from abc import ABC, ABCMeta, abstractmethod
 
-from hx_lti_initializer.models import LTICourse, LTIProfile
+from hx_lti_initializer.models import LTICourse
 from target_object_database.models import TargetObject
 
 
@@ -30,7 +30,7 @@ class TOD_Implementation(TODAPI_LTI):
     @staticmethod
     def get_own_targets_from_course(course_id):
         """
-        This function should return a list of objects with the attributes set up above. 
+        This function should return a list of objects with the attributes set up above.
         """
         course_for_user = LTICourse.get_course_by_id(course_id)
         response = TargetObject.objects.filter(target_courses=course_for_user).order_by(
@@ -51,7 +51,7 @@ class TOD_Implementation(TODAPI_LTI):
     @staticmethod
     def get_own_targets_as_user(user_requesting):
         """
-        This function should return all the files 
+        This function should return all the files
         """
         courses_for_user = LTICourse.objects.filter(course_admins=user_requesting.id)
         files_in_courses = []
